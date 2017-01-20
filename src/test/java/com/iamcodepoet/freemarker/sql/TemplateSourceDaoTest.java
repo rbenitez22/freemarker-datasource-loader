5f5e8f7f0556f43c22d667bac0a1d629fae88d1c
Roberto C. Benitez <rbenitez22@yahoo.com>
2017-01-20 16:55:41 -0600

    added insert/query test code. added connection method/table creator

--- src/test/java/com/iamcodepoet/freemarker/sql/TemplateSourceDaoTest.java
+++ src/test/java/com/iamcodepoet/freemarker/sql/TemplateSourceDaoTest.java
@@ -5,8 +5,14 @@
  */
 package com.iamcodepoet.freemarker.sql;
 
+import java.io.File;
+import java.nio.file.Paths;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 import org.junit.Test;
-import static org.junit.Assert.*;
+import org.junit.BeforeClass;
 
 /**
  *
@@ -18,14 +24,77 @@ public class TemplateSourceDaoTest
     public final static String DB_NAME="template-loader.db";
     public final static String TABLE_NAME="templates";
     
+    private static String DbUrl;
+    private static JdbcMetaData metadata;
     
     public TemplateSourceDaoTest()
     {
     }
-
-    @Test
-    public void testSomeMethod()
+    
+    @BeforeClass public static void init()
     {
+        DbUrl = Paths.get(URL_PREFIX,new File("").getAbsolutePath(), DB_NAME).toString();
+        metadata = new JdbcMetaData(TABLE_NAME);
+        
+        try(Connection conn = getConnection())
+        {
+            createTableIfNotExists(conn);
+        }
+        catch (Exception e) 
+        {
+            e.printStackTrace();
+        }
+    }
+    
+     private static void createTableIfNotExists(Connection conn) throws SQLException
+    {
+        String sql="CREATE TABLE IF NOT EXISTS templates\n" +
+                    "(\n" +
+                    "	id INTEGER PRIMARY KEY,\n" +
+                    "	templateName TEXT,\n" +
+                    "	templateSource TEXT,\n" +
+                    "	dateCreated INTEGER,\n" +
+                    "	lastModified INTEGER,\n" +
+                    "	locale TEXT\n" +
+                    ")";
+        
+        try(Statement stmt=conn.createStatement())
+        {
+            stmt.execute(sql);
+        }
+    }
+     
+    @Test
+    public void testInsert() throws Exception
+    {
+        try (TemplateSourceDao dao= new TemplateSourceDao(getConnection(), metadata))
+        {
+            JdbcTemplateSource templateSource = new JdbcTemplateSource();
+            
+            templateSource.setName("Cover Letter Template");
+            templateSource.setSource("Dear ${hiringManager},\n\t You hire me.  You hire me now!");
+            
+            dao.insert(templateSource);
+            
+        }
+        
+    }
+    
+    @Test
+    public void testQuery() throws Exception
+    {
+        try (TemplateSourceDao dao= new TemplateSourceDao(getConnection(), metadata))
+        {
+            
+            dao.query()
+                    .forEach(e->System.out.printf("[%s] -> %s\n",e.getName(),e.getSource()));
+        }
+        
+    }
+    
+    private static Connection getConnection() throws SQLException
+    {
+        return DriverManager.getConnection(DbUrl);
     }
     
 }
