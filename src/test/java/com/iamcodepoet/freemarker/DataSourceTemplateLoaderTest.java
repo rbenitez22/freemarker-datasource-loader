6b09a02d631fac679d89d668e2b60aa00da07281
Roberto C. Benitez <rbenitez22@yahoo.com>
2017-01-21 14:52:55 -0600

    updated to use Config propvider class

--- src/test/java/com/iamcodepoet/freemarker/DataSourceTemplateLoaderTest.java
+++ src/test/java/com/iamcodepoet/freemarker/DataSourceTemplateLoaderTest.java
@@ -1,10 +1,23 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/* 
+ * Copyright (C) 2017 Roberto C. Benitez
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  */
 package com.iamcodepoet.freemarker;
 
+import com.iamcodepoet.freemarker.sql.JdbcMetaData;
 import freemarker.core.ParseException;
 import freemarker.template.Configuration;
 import freemarker.template.MalformedTemplateNameException;
@@ -17,13 +30,12 @@ import java.io.StringWriter;
 import java.nio.file.Paths;
 import java.sql.SQLException;
 import java.util.HashMap;
-import java.util.Locale;
 import java.util.Map;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-import org.junit.AfterClass;
+import java.util.Properties;
+import javax.sql.DataSource;
 import org.junit.Test;
 import org.junit.BeforeClass;
+import org.postgresql.ds.PGSimpleDataSource;
 import org.sqlite.SQLiteDataSource;
 
 /**
@@ -37,6 +49,7 @@ public class DataSourceTemplateLoaderTest
     public final static String TABLE_NAME="templates";
     
     private static String jdbcUrl;
+    private static ConfigProvider config;
     
     public DataSourceTemplateLoaderTest()
     {
@@ -48,6 +61,7 @@ public class DataSourceTemplateLoaderTest
         try
         {
             jdbcUrl = Paths.get(URL_PREFIX,file.getAbsolutePath(), DB_NAME).toString();
+            config = new ConfigProvider();
             
         }
         catch(Exception e)
@@ -55,43 +69,96 @@ public class DataSourceTemplateLoaderTest
             throw new RuntimeException(e);
         }
     }
-     
+    
     @Test public void testLoader() throws MalformedTemplateNameException, ParseException, IOException, TemplateException, SQLException
     {
         
             
-            SQLiteDataSource ds= new SQLiteDataSource();
-            ds.setUrl(jdbcUrl);
-            DataSourceTemplateLoader loader= new DataSourceTemplateLoader(ds,TABLE_NAME);
+            javax.sql.DataSource ds= createPostgreSqlDataSource(); //createSqLiteDataSource();
+            JdbcMetaData meta  = new JdbcMetaData("public", TABLE_NAME);
+            DataSourceTemplateLoader loader= new DataSourceTemplateLoader(ds,meta);
 
             
             Version version = new Version(2, 3, 23);
             Configuration config= new Configuration(version);
             config.setTemplateLoader(loader);
             
-            Template template1=config.getTemplate("Profound Message");
+            testTemplate1(config);
             
+            testTemplate2(config);
+      
+    }
+
+    private boolean testTemplate2(Configuration config) throws TemplateException, IOException
+    {
+        try
+        {
+            Template template2 = config.getTemplate("Cover Letter Template");
+            HashMap<String,Object> model = new HashMap<>();
+            model.put("hiringManager", "Miss Ada Byron");
+
+            System.out.printf("--%s\n",template2.getName());
+            StringWriter writer2 = new StringWriter();
+            template2.process(model, writer2);
+            System.out.printf("Cover Letter: %s\n\t",writer2.toString());
+            
+            return true;
+        }
+         catch(IOException | TemplateException e)
+        {
+            Throwable root= (e.getCause() == null)?e : e.getCause();
+            System.out.println(root.getMessage());
+            return false;
+        }
+    }
+
+    private boolean testTemplate1(Configuration config) throws TemplateException, IOException
+    {
+        try
+        {
+            Template template1=config.getTemplate("Profound Message");
+
             Map<String,String> model= new HashMap<>();
             model.put("noun1", "Pinguins");
             model.put("verb", "waddle");
             model.put("noun2", "aid");
             model.put("noun3", "Linux distro");
-            
+
             StringWriter writer1= new  StringWriter();
-            
+
             template1.process(model, writer1);
-            
+
             System.out.println("Message test: " + writer1.toString());
             
-            Template template2 = config.getTemplate("Cover Letter Template");
-            model = new HashMap<>();
-            model.put("hiringManager", "Miss Ada Byron");
-            
-            System.out.printf("--%s\n",template2.getName());
-            StringWriter writer2 = new StringWriter();
-            template2.process(model, writer2);
-            System.out.printf("Cover Letter: %s\n\t",writer2.toString());
-      
+            return true;
+        }
+        catch(IOException | TemplateException e)
+        {
+            Throwable root= (e.getCause() == null)?e : e.getCause();
+            System.out.println(root.getMessage());
+            return false;
+        }
+    }
+
+    private SQLiteDataSource createSqLiteDataSource()
+    {
+        SQLiteDataSource ds= new SQLiteDataSource();
+        ds.setUrl(jdbcUrl);
+        return ds;
+    }
+    
+    private DataSource createPostgreSqlDataSource() throws IOException
+    {
+        PGSimpleDataSource ds=  new PGSimpleDataSource();
+        
+        Properties props = config.getConfig("postgresql-sandbox");
+        
+        ds.setServerName(props.getProperty("host"));
+        ds.setDatabaseName(props.getProperty("databaseName"));
+        ds.setUser(props.getProperty("user"));
+        ds.setPassword(props.getProperty("password"));
+        
+        return ds;
     }
 
    
