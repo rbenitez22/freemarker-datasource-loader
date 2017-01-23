20fd91abb0385c6180092f5437b8a5db14ec6eb5
Roberto C. Benitez <rbenitez22@yahoo.com>
2017-01-22 19:58:10 -0600

    new classes

---
+++ src/main/java/com/iamcodepoet/freemarker/TemplateName.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright 2017 Roberto C. Benitez.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.iamcodepoet.freemarker;
+
+import com.iamcodepoet.freemarker.util.Experimental;
+import java.util.Locale;
+
+/**
+ *A specification for a (localizable) template name.
+ * @author Roberto C. Benitez
+ */
+@Experimental(description = "Should this be more generic (e.g. LocalizableName)?")
+public interface TemplateName
+{
+    String getName();
+    String getLocalizedName();
+    Locale getLocale();
+    
+}
